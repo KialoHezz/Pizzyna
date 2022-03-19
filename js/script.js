29080a997b4b178c0f4ac1f6a362bf67b1c66f4c
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-19 13:16:21 +0300

    In section of Service I've made a toggle between image and paragraph,and add information to show the user to click to read and to know the prices

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    $("body").css("background:black");
-})
+$(document).ready(function () {
+  $(".toggle").click(function () {
+    $("#smallImgToggle").toggle();
+    $("#HiddenParagraph").toggle();
+  });
+  $("#secontoggle").click(function () {
+    $("#secondToggle-img").toggle();
+    $("#Paragraphhidden").toggle();
+  });
+  $(".thirdToggle").click(function(){
+      $("#thirdImageToggle").toggle();
+      $("#hiddenThirdParagraph").toggle();
+  });
+});
